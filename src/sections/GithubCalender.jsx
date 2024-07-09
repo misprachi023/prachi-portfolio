07bf3f757d73fa07f0cbee149940ceaf066f943e
misprachi023 <prachimishra23123@gmail.com>
2024-07-09 12:16:23 +0530

    add project

--- src/sections/GithubCalender.jsx
+++ src/sections/GithubCalender.jsx
@@ -6,34 +6,45 @@ export default function GithubCalender() {
   return (
     <>
       <h1 className="sub-title">
-				GitHub <span>Stats</span>
-			</h1>
-     
-     <Box display={['block', 'block', 'flex', 'flex', 'flex']} margin={'auto'} width={"80%"}>
-      <Box width={'50%'}>
-        <img
-          id="github-top-langs"
-          width="100%"
-          src="https://github-readme-stats.vercel.app/api/top-langs?username=misprachi023&theme=transparent&hide_border=true&show_icons=true&locale=en&layout=compact&title_color=white&text_color=#32de84"
-        />
-      </Box>
-      <Box width={['50%', '90%', '50%', '50%', '50%']}>
-        <img
-          id="github-stats-card"
-          width="100%"
-          src="https://github-readme-stats.vercel.app/api?username=misprachi023&theme=transparent&hide_border=true&show_icons=true&locale=en&title_color=white&text_color=#32de84"
-        />
-      </Box>
-      
-      </Box>
-      <Box width={['100%', '90%', '50%', '50%', '50%']} margin={'auto'} >
-        <img
-          id="github-streak-stats"
-          width="100%"
-          src="https://github-readme-streak-stats.herokuapp.com/?user=misprachi023&theme=transparent&hide_border=true&title_color=white&text_color=#32de84&sideNums=white&sideLabels=white&ring=white&fire=white&currStreakLabel=white&currStreakNum=white"
-          alt=""
-        />
-      </Box>
+        GitHub <span>Stats</span>
+      </h1>
+
+      <Flex justifyContent="center" alignItems="center" flexWrap="wrap" padding={2}>
+        {/* GitHub Top Languages */}
+        <Box width={['50%', '40%']} marginBottom={4}>
+          <img
+            id="github-top-langs"
+            width="100%"
+            src="https://github-readme-stats.vercel.app/api/top-langs?username=misprachi023&theme=transparent&hide_border=true&show_icons=true&locale=en&layout=compact&title_color=white&text_color=#32de84"
+            alt="GitHub Top Languages"
+          />
+        </Box>
+
+        {/* GitHub Stats Card */}
+        <Box width={['60%', '40%']} marginBottom={4}>
+          <img
+            id="github-stats-card"
+            width="100%"
+            src="https://github-readme-stats.vercel.app/api?username=misprachi023&theme=transparent&hide_border=true&show_icons=true&locale=en&title_color=white&text_color=#32de84"
+            alt="GitHub Stats Card"
+          />
+        </Box>
+
+        {/* GitHub Streak Stats */}
+        <Box width={['50%']} marginBottom={4}>
+          <img
+            id="github-streak-stats"
+            width="100%"
+            src="https://github-readme-streak-stats.herokuapp.com/?user=misprachi023&theme=transparent&hide_border=true&title_color=white&text_color=#32de84&sideNums=white&sideLabels=white&ring=white&fire=white&currStreakLabel=white&currStreakNum=white"
+            alt="GitHub Streak Stats"
+          />
+        </Box>
+
+        {/* GitHub Contribution Calendar */}
+        <Box width={['70%']} marginBottom={4} >
+          <GitHubCalendar username="misprachi023" />
+        </Box>
+      </Flex>
     </>
   );
 }
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,216 +1,104 @@
 import React from "react";
-
-import { Button, Image } from "@chakra-ui/react";
-import { Box, Flex } from "@chakra-ui/react";
-import { Card, Link, Badge, Heading } from "@chakra-ui/react";
-import HoverVideoPlayer from "react-hover-video-player";
-// import { Player } from "video-react";
 import videoairtable from "../airtable.mp4";
 import boatlife from "../boatlifestyle.mp4";
-import { List, ListItem, Text } from "@chakra-ui/react";
+import questlab from "../quest-labs.mp4";
+import resturant from "../resturant.mp4";
+import { Button, Box, Flex, Card, Link, Text, Heading, List, ListItem, Grid } from "@chakra-ui/react";
 
-/**
- * @author
- * @function Projects
- **/
+const projectsData = [
+  {
+    videoSrc: videoairtable,
+    description: "Airtable is a cloud-based collaborative spreadsheet app that allows you to create, organize, and share spreadsheets.",
+    gitHubLink: "https://github.com/misprachi023/Airtable",
+    liveDemoLink: "https://aritable.vercel.app/",
+    techStack: ["HTML", "CSS", "React", "JavaScript"]
+  },
+  {
+    videoSrc: boatlife,
+    description: "BoatLifeStyle is an e-commerce web application that allows you to buy and sell a collection of Earbuds, Headphones.",
+    gitHubLink: "https://github.com/misprachi023/BoatLifeStyle",
+    liveDemoLink: "https://boat-life-style-nine.vercel.app/",
+    techStack: ["React", "Node.js", "Express.js", "MongoDB"]
+  },
+  {
+    videoSrc: questlab,
+    description: "Transform Your Product with Intelligent In-App Experiences Empower growth & Marketing Teams to Boost Engagement ",
+    gitHubLink: "https://github.com/misprachi023/Questlabs",
+    liveDemoLink: "https://questlabs-blush.vercel.app/",
+    techStack: ["HTML", "CSS", "React", "JavaScript"]
+  },
+  {
+    videoSrc: resturant,
+    description: "Restaurant is an e-commerce web application that allows you to buy and sell a collection of food items.",
+    gitHubLink: "https://github.com/misprachi023/Restaurant",
+    liveDemoLink: "https://restaurant-beta-livid.vercel.app/",
+    techStack: ["HTML", "CSS", "React", "JavaScript"]
+  }
+];
 
 export const Projects = (props) => {
-  var settings = {
-    dots: false,
-    infinite: true,
-    speed: 100,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    arrow: true,
-    pauseOnHover: false,
-  };
   return (
-    <Box id="projects">
-      <Heading
-        className="main-text"
-        textAlign={"center"}
-        id="project"
-        size={["sm", "md", "lg", "lg", "xl", "3xl"]}
-      >
+    <Box id="projects" padding="20px">
+      <Heading className="main-text" textAlign="center" id="project" size={["sm", "md", "lg", "lg", "xl", "3xl"]} marginBottom="20px">
         <div className="about-text">
           <h2>
             Latest <span>Project</span>
           </h2>
         </div>
       </Heading>
-      <Card
+      <Grid
+        templateColumns={["1fr", "1fr", "1fr 1fr"]}
+        gap="30px"
         width={["90%", "90%", "80%", "70%", "60%"]}
-        backgroundColor={"#081b29"}
-        margin={"auto"}
-        size={"lg"}
-        display="flex"
-        gap={"30px"}
-        justifyContent="space-between"
-        flexDirection={["column", "column", "column", "row", "row"]}
+        backgroundColor="#081b29"
+        margin="auto"
+        padding="20px"
+        borderRadius="10px"
       >
-        <div style={{ flex: "1", marginRight: "10px", border:'5px solid cyan', borderRadius:'10px'}}>
-          <video
-            autoPlay
-            loop
-            muted
-            style={{ width: "100%", height: "auto", borderRadius: "10px" }}
-          >
-            <source src={videoairtable} type="video/mp4" />
-          </video>
+        {projectsData.map((project, index) => (
           <Box
-            borderRadius={"30px"}
-            backgroundColor={"#051129"}
-            padding={"10px"}
-            marginBottom="10px"
+            key={index}
+            border="5px solid cyan"
+            borderRadius="10px"
+            padding="10px"
+            backgroundColor="#051129"
           >
-            <Text
-              fontSize={"25px"}
-              backgroundColor={"#051129"}
-              color={"white"}
-              borderRadius={"10px"}
-            >
-              Description
-            </Text>
-            <Text
-              backgroundColor={"#051129"}
-              borderRadius={"10px"}
-              color={"white"}
-            >
-              Airtable is a cloud-based collaborative spreadsheet app that
-              allows you to create, organize, and share spreadsheets.
-            </Text>
+            <video autoPlay loop muted style={{ width: "100%", height: "auto", borderRadius: "10px" }}>
+              <source src={project.videoSrc} type="video/mp4" />
+            </video>
+            <Box borderRadius="10px" backgroundColor="#051129" padding="10px" marginBottom="10px">
+              <Text fontSize="25px" color="white" borderRadius="10px">
+                Description
+              </Text>
+              <Text borderRadius="10px" color="white">
+                {project.description}
+              </Text>
+            </Box>
+            <Box textAlign="center" padding="10px" display="flex" justifyContent="space-around" marginBottom="10px">
+              <Link isExternal href={project.gitHubLink}>
+                <Button backgroundColor="cyan" _hover={{ backgroundColor: "white" }} color="#051129" variant="outline">
+                  Git Hub
+                </Button>
+              </Link>
+              <Link isExternal href={project.liveDemoLink}>
+                <Button backgroundColor="cyan" _hover={{ backgroundColor: "white" }} color="#051129" variant="outline">
+                  Live Demo
+                </Button>
+              </Link>
+            </Box>
+            <Box padding="10px" borderRadius="10px">
+              <Text textAlign="center" fontSize="25px" color="white">
+                Tech Stack
+              </Text>
+              <List textAlign="center" color="white">
+                {project.techStack.map((tech, index) => (
+                  <ListItem key={index}>{tech}</ListItem>
+                ))}
+              </List>
+            </Box>
           </Box>
-          <Box
-            textAlign={"center"}
-            padding={"10px"}
-            display={"flex"}
-            justifyContent={"space-around"}
-            marginBottom="10px"
-          >
-            <Link
-              isExternal={true}
-              href="https://github.com/misprachi023/Airtable"
-            >
-              <Button
-                backgroundColor={"cyan"}
-                _hover={{ backgroundColor: "white" }}
-                color={"#051129"}
-                variant="outline"
-              >
-                Git Hub
-              </Button>
-            </Link>
-            <Link isExternal={true} href="https://aritable.vercel.app/">
-              <Button
-                backgroundColor={"cyan"}
-                _hover={{ backgroundColor: "white" }}
-                color={"#051129"}
-                variant="outline"
-              >
-                Live Demo
-              </Button>
-            </Link>
-          </Box>
-          <Box
-            backgroundColor={"#051129"}
-            padding={"10px"}
-            borderRadius={"30px"}
-          >
-            <Text textAlign={"center"} fontSize={"25px"} color={"white"}>
-              Tech Stack
-            </Text>
-            <List textAlign={"center"} color={"white"}>
-              <ListItem>HTML</ListItem>
-              <ListItem>CSS</ListItem>
-              <ListItem>React</ListItem>
-              <ListItem>JavaScript</ListItem>
-            </List>
-          </Box>
-        </div>
-        <div style={{ flex: "1", border:'5px solid cyan', borderRadius:'10px'  }}>
-          <video
-            autoPlay
-            loop
-            muted
-            style={{ width: "100%", height: "auto", borderRadius: "10px" }}
-          >
-            <source src={boatlife} type="video/mp4" />
-          </video>
-          <Box
-            borderRadius={"30px"}
-            backgroundColor={"#051129"}
-            padding={"10px"}
-            marginBottom="10px"
-          >
-            <Text
-              fontSize={"25px"}
-              backgroundColor={"#051129"}
-              color={"white"}
-              borderRadius={"10px"}
-            >
-              Description
-            </Text>
-            <Text
-              backgroundColor={"#051129"}
-              borderRadius={"10px"}
-              color={"white"}
-            >
-              BoatLifeStyle is an e-commerce web application that allows you to
-              buy and sell collection of Earbuds, Headphones.
-            </Text>
-          </Box>
-          <Box
-            textAlign={"center"}
-            padding={"10px"}
-            display={"flex"}
-            justifyContent={"space-around"}
-            marginBottom="10px"
-          >
-            <Link
-              isExternal={true}
-              href="https://github.com/misprachi023/BoatLifeStyle"
-            >
-              <Button
-                backgroundColor={"cyan"}
-                _hover={{ backgroundColor: "white" }}
-                color={"#051129"}
-                variant="outline"
-              >
-                Git Hub
-              </Button>
-            </Link>
-            <Link
-              isExternal={true}
-              href="https://boat-life-style-nine.vercel.app/"
-            >
-              <Button
-                backgroundColor={"cyan"}
-                _hover={{ backgroundColor: "white" }}
-                color={"#051129"}
-                variant="outline"
-              >
-                Live Demo
-              </Button>
-            </Link>
-          </Box>
-          <Box
-            backgroundColor={"#051129"}
-            padding={"10px"}
-            borderRadius={"30px"}
-          >
-            <Text textAlign={"center"} fontSize={"25px"} color={"white"}>
-              Tech Stack
-            </Text>
-            <List textAlign={"center"} color={"white"}>
-              <ListItem>React</ListItem>
-              <ListItem>Node js</ListItem>
-              <ListItem>Express js</ListItem>
-              <ListItem>MongoDB</ListItem>
-            </List>
-          </Box>
-        </div>
-      </Card>
+        ))}
+      </Grid>
     </Box>
   );
 };
